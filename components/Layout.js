bd6842643142f5add2637f5243ead664cbb73742
oman1219 <32309792+oman1219@users.noreply.github.com>
2021-10-20 17:42:43 -0400

    Styled For Mobile Only. Work in progress features hidden

--- components/Layout.js
+++ components/Layout.js
@@ -65,7 +65,7 @@ export default function Layout({children}){
         </Link>
         <div className="w-full flex flex-row items-center justify-center ">
           <a target="_blank" rel="noreferrer" className="p-4" href={`https://www.instagram.com/daytonlocalmusic`}>
-            <svg height="30" width="30" viewBox={"0 0 24 24"} fill={"#616161"}>
+            <svg height="48" width="48" viewBox={"0 0 24 24"} fill={"#616161"}>
               <path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"/>
             </svg>
           </a>
