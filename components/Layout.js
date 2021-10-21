579a4bb4e0c7984c2f8700938a862431675188b9
oman1219 <32309792+oman1219@users.noreply.github.com>
2021-10-21 18:25:31 -0400

    accessibility improvements

--- components/Layout.js
+++ components/Layout.js
@@ -41,7 +41,7 @@ export default function Layout({children}){
         )}
         </nav>
         {clicked &&(
-        <nav className="fixed  top-16 left-0 z-10  text-base text-left font-bold  w-full h-auto shadow-sm border-b bg-white divide-y  divide-solid flex flex-col">
+        <nav className="fixed top-16 left-0 z-10  text-base text-left font-bold  w-full h-auto shadow-sm border-b bg-white divide-y  divide-solid flex flex-col">
           <Link href={'/'} ><a className="pl-6 py-2">Home</a></Link>
         </nav>
         )}
@@ -53,28 +53,24 @@ export default function Layout({children}){
         <nav className="w-full flex flex-row items-center justify-center ">
           <Link href={'/'} ><a className="px-2">Home</a></Link>
         </nav>
-        <Link href={'/'}>
-          <a >
+          <small >
             &copy; 2021 DAYTON LOCAL MUSIC
-          </a>
-        </Link>
-        <Link href={'/'}>
-          <a >
+          </small>
+          <small >
             Built In Dayton, Ohio By Theo Gainey
-          </a>
-        </Link>
+          </small>
         <div className="w-full flex flex-row items-center justify-center ">
-          <a  target="_blank" rel="noreferrer" aria-label="Link To Instagram" className="p-4" href={`https://www.instagram.com/daytonlocalmusic`}>
+          <a  target="_blank" rel="noreferrer" aria-label="Link To Instagram" className="m-1 p-4" href={`https://www.instagram.com/daytonlocalmusic`}>
             <svg height="48" width="48" viewBox={"0 0 24 24"} fill={"#616161"}>
               <path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"/>
             </svg>
           </a>
-          <a aria-label="Link To Facebook" target="_blank" rel="noreferrer" className="p-4" href={`https://www.facebook.com/daytonlocalmusic`}>
+          <a aria-label="Link To Facebook" target="_blank" rel="noreferrer" className="m-1 p-4" href={`https://www.facebook.com/daytonlocalmusic`}>
             <svg height="48" width="48" viewBox={"0 0 24 24"} fill={"#616161"}>
               <path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"/>
             </svg>
           </a>
-          <a aria-label="Link To Twitter" target="_blank" rel="noreferrer" className="p-4" href={`https://twitter.com/DaytonLocalMus`}>
+          <a aria-label="Link To Twitter" target="_blank" rel="noreferrer" className="m-1 p-4" href={`https://twitter.com/DaytonLocalMus`}>
             <svg height="48" width="48" viewBox={"0 0 24 24"} fill={"#616161"}>
               <path d="M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z"/>
             </svg>
--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,11 +1,10 @@
-// pages/api/auth/[...auth0].js
 import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
 
 
 export default handleAuth({
   async callback(req, res) {
     try {
-      await handleCallback(req, res, { redirectUri: `${process.env.AUTH0_BASE_URL}/dashboard` });
+      await handleCallback(req, res, { redirectUri: `${process.env.AUTH0_BASE_URL}` });
     } catch (error) {
       res.status(error.status || 500).end(error.message);
     }
